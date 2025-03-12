55784b279e4c71886cbb3a992dfa89d8047909d3
M.ghalevand <m.ghalevand@outlook.com>
2025-03-12 12:26:57 +0330

    Add ESLint configuration with TypeScript and Prettier support
    This commit introduces an ESLint configuration file (`eslint.config.mjs`) tailored for TypeScript projects. It includes plugins for TypeScript (`@typescript-eslint`), Prettier, and import management, along with specific rules such as import ordering and no-unused-vars enforcement. JavaScript files are restricted to encourage TypeScript usage.

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+export default [
+  {
+    ignores: ['**/node_modules', '**/dist', '**/public', '**/*.md', '**/.idea']
+  },
+  ...fixupConfigRules(compat.extends('plugin:import/typescript', 'plugin:import/warnings')),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      import: fixupPluginRules(_import),
+      prettier: fixupPluginRules(prettier)
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      }
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx']
+        }
+      }
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false
+        }
+      ],
+      'no-console': 'error',
+      'prefer-const': 'error',
+
+      "import/order": [
+        "warn",
+        {
+          "newlines-between": "always",
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          },
+          "groups": [
+            ["builtin", "external"],
+            ["internal", "parent", "sibling", "index"]
+          ]
+        }
+      ],
+      'prettier/prettier': 'error',
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'const', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'export' },
+        { blankLine: 'always', prev: '*', next: 'return' }
+      ]
+    }
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'Program',
+          message: 'Only TypeScript (.ts, .tsx) files are allowed.'
+        }
+      ]
+    }
+  }
+];
