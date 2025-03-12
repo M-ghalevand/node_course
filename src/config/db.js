99d671f6f17bb57cb84803b8f3ed07dc0be263d9
M.ghalevand <m.ghalevand@outlook.com>
2025-03-12 10:51:58 +0330

    Refactor database connection to use pg Pool with SSL.
    Replaced the pg Client implementation with Pool for connection pooling and added SSL configuration for secure connections. This improves performance and ensures compatibility with environments like Heroku.

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,16 @@
-import pg from 'pg';
+import pkg from 'pg';
+
+const {Pool} = pkg;
+
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { Client } = pg;
 
-const client = new Client({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+export const dbPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-
-await client.connect();
-export default client;
