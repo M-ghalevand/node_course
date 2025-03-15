25ab3ec35f29b348773ffe80b03f34e839f318e8
M.ghalevand <m.ghalevand@outlook.com>
2025-03-15 11:07:44 +0330

    Refactor server to use Express and add Swagger support.
    Replaced the custom HTTP server with an Express-based implementation for better structure and scalability. Introduced Swagger documentation setup for easier API exploration and testing. Updated routing to use modularized route definitions.

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+import app from './app';
+import { setupSwagger } from './docs/swagger';
+
+const PORT = process.env.PORT || 3000;
+
+setupSwagger(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+});
