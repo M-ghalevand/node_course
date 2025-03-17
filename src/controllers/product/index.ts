0628248964f82836e1025e2066c79352e69c204e
M.ghalevand <m.ghalevand@outlook.com>
2025-03-17 09:41:11 +0330

    Add index file for product controllers
    Created an index file to consolidate exports for product controllers. This simplifies imports elsewhere in the codebase by providing a single entry point for all product-related controllers.

---
+++ src/controllers/product/index.ts
@@ -0,0 +1,9 @@
+export * from './create-product.controller';
+
+export * from './delete-product.controller';
+
+export * from './get-product-by-id.controller';
+
+export * from './get-products.controller';
+
+export * from './update-product.controller';
