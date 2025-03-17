229e74ba2e0a5dc35eccfd8a31c99616ca920d51
M.ghalevand <m.ghalevand@outlook.com>
2025-03-17 09:40:17 +0330

    Remove product route handlers and related exports
    This commit deletes all product-related route handlers, including creation, deletion, updates, retrieval by ID, and retrieval of all products. Additionally, it removes the corresponding exports in the product routes index file. This cleanup may indicate a change in approach or deprecation of these API functionalities.

--- src/routes/product/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './create-product-route';
-
-export * from './delete-product-route';
-
-export * from './get-product-by-id-route';
-
-export * from './get-products-route';
-
-export * from './update-product-route';
