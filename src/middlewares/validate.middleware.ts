f6f4b825c25d04c2d8e280647a1e75770395b5c1
M.ghalevand <m.ghalevand@outlook.com>
2025-03-17 10:03:38 +0330

    Refactor validate middleware for improved readability.
    Reformatted the validate middleware to enhance code clarity and maintainability. Added an explicit return statement after sending a response to ensure proper flow control. This change has no impact on functionality but improves code structure.

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,23 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject, ZodError } from 'zod';
 
-export const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
-  try {
-    schema.parse({
-      body: req.body,
-      query: req.query,
-      params: req.params
-    });
-    next();
-  } catch (error) {
-    if (error instanceof ZodError) {
-      return res.status(400).json({
-        errors: error.errors.map((err) => ({
-          field: err.path.join('.'),
-          message: err.message
-        }))
+export const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params
       });
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          errors: error.errors.map((err) => ({
+            field: err.path.join('.'),
+            message: err.message
+          }))
+        });
+
+        return;
+      }
+      next(error);
     }
-    next(error);
-  }
-};
+  };
