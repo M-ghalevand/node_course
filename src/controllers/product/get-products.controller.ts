ddec13d673d4233e2bf303db1e53f824a3edfde3
M.ghalevand <m.ghalevand@outlook.com>
2025-03-17 09:40:58 +0330

    Add getProductsController to handle product retrieval
    Introduced a new controller to manage the retrieval of products using the `getProducts` model function. Includes error handling with proper response status and message conversion using the `String` constructor.

---
+++ src/controllers/product/get-products.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+import { getProducts } from '../../models/product';
+
+export const getProductsController = async (req: Request, res: Response) => {
+  try {
+    const users = await getProducts();
+
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: String(err) }); // Use String constructor with `new`
+  }
+};
