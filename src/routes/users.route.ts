a3332e4e5614d5bc60420a17fde936d5873e47f4
M.ghalevand <m.ghalevand@outlook.com>
2025-03-17 10:03:08 +0330

    Add validation to user creation endpoint
    Integrate middleware to validate input for the user creation route using `createUserValidator`. This ensures only valid data is processed, improving API reliability and error handling.

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,12 @@
 import { createUserController, deleteUserController, getAllUsersController, getUserByIdController } from 'controllers';
 import { Router } from 'express';
+import { validate } from 'middlewares';
+import { createUserValidator } from 'validators';
 
 const usersRouter = Router();
 
 usersRouter.get('/', getAllUsersController);
-usersRouter.post('/', createUserController);
+usersRouter.post('/', validate(createUserValidator), createUserController);
 usersRouter.get('/:id', getUserByIdController);
 usersRouter.delete('/:id', deleteUserController);
 
